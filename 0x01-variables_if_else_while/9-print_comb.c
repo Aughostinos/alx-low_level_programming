8cf47a798d4376d0c6ad38dc28d52fa36ad090f6
Aughostinos <augustinos.nabil@gmail.com>
2023-08-11 16:31:18 -0700

    putchar(,);

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description:
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar('\,');
+		putchar('\ ');
+	}
+	putchar('\n');
+	return (0);
+}
