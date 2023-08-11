bd9abc4ffa220ab0736d56c6db05aa784ae1563c
Aughostinos <augustinos.nabil@gmail.com>
2023-08-11 16:21:05 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: C prgrams
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 102)
+	{
+		putchar(i);
+		if (i == 57)
+			i += 39;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
