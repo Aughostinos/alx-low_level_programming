94acd46d7881503af9d6c9acadbdf40952eba80f
Aughostinos <augustinos.nabil@gmail.com>
2023-08-10 19:00:21 -0700

    full

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: C program
+ *
+ * Return: 1 (!success)
+*/
+
+int main(void)
+{
+	char x []= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,x,59);
+	return (1);
+}
