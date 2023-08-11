ef23d2d7e5fca0bcac68eef9e7c97501df7956d4
Aughostinos <augustinos.nabil@gmail.com>
2023-08-11 15:14:29 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: C prgrams
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+	putchar(i);
+	putchar('\n');
+	i++;
+	}
+	return (0);
+}
