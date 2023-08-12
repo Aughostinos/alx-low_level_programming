d2a6be3d25206a9a6152db8783b946ea54476c6f
Aughostinos <augustinos.nabil@gmail.com>
2023-08-11 17:41:06 -0700

    putchar

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description:
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n, i;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (l = 50; l <= 57; l++)
+			{
+				if (n != i && n < i)
+				{
+					putchar(n);
+					putchar(i);
+					putchar(l);
+					if (n != 55 || i != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
