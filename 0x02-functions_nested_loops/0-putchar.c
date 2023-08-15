58eda16ceffab20c645b46bcbeaa50f1d5b04f53
Aughostinos <augustinos.nabil@gmail.com>
2023-08-15 12:18:57 -0700

    c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@
  * Return: Always 0 (Success)
 */
 
-int _putchar(char c)
+int _putchar(void)
 {
 	char str[] = "_putchar";
 	int i;
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,5 @@
  *
  * Return: Always 0 (Success)
 */
-int _putchar(char c);
+int _putchar(void);
 #endif
