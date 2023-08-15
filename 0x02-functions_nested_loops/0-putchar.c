5fd99dc379bca24d2cff15d1efd3e444bc7263f6
Aughostinos <augustinos.nabil@gmail.com>
2023-08-15 05:30:52 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program
+ *
+ * Return: Always 0 (Success)
+*/
+
+//Task 0
+int main(void)
+{
+        printf("_putchar\n");
+      
+        return (0);
+}
