a8e29c03526ef7295a42ef741650be17c2e6cce6
Aughostinos <augustinos.nabil@gmail.com>
2023-08-15 15:08:07 -0700

    task3

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - function checks if the input is letter or not
+ * 
+ * @c: the input of the function
+ * 
+ * Return: 1 if the input is a letter and 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
