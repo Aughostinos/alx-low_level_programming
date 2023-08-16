84e7b62976154c12b24c58966b0fa812ed0f56de
Aughostinos <augustinos.nabil@gmail.com>
2023-08-15 17:03:23 -0700

    task12

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry pint
+ * Return:
+*/
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
+
