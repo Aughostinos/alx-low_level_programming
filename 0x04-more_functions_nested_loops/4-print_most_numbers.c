acda6ac2df51e066b9a21493f998a3ea2bc99195
Aughostinos <augustinos.nabil@gmail.com>
2023-08-17 13:12:03 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9
+ * Return: void
+*/
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(n);
+	}
+	_putchar('\n');
+}
