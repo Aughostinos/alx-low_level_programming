fa615752bbd321c586b84209f7135b731b6b781c
Aughostinos <augustinos.nabil@gmail.com>
2023-08-17 08:47:19 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - function that check if the letter in upper case or lowercase
+ * @c: the input char
+ * Return: 1 if c is upper case 0 if c is lwoer case
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && C <= 90)
+		reutun (1);
+	else if (c >= 97 && <= 122)
+		return (0);
+	return (0);
+}
