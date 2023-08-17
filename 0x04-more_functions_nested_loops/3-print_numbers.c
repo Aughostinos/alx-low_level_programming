764a7e75c3502f7d277288438c57b55495d00582
Aughostinos <augustinos.nabil@gmail.com>
2023-08-17 12:52:05 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints the numbers, from 0 to 9
+ * Return: 0
+*/
+print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		_putchar(n);
+	_putcar("\n");
+}
