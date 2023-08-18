21f01386ec1f7009160b8deb7e848101ba5d0b3a
Aughostinos <augustinos.nabil@gmail.com>
2023-08-17 17:38:18 -0700

    11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,48 +1,39 @@
-#include "main.h"
 #include <stdio.h>
 #include <math.h>
 
 /**
- * is_prime - finds and prints the largest prime factor
- * @n: input
-*/
-int is_prime(long long n) {
-    if (n <= 1) {
-        return 0;
-    }
-    if (n <= 3) {
-        return 1;
-    }
-    if (n % 2 == 0 || n % 3 == 0) {
-        return 0;
-    }
-    
-    for (long long i = 5; i * i <= n; i += 6) {
-        if (n % i == 0 || n % (i + 2) == 0) {
-            return 0;
-        }
-    }
-    return 1;
+ * main - finds and prints the largest prime factor
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int n;
+	long int max;
+	long int i;
+
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
+	return (0);
 }
 
-long long largest_prime_factor(long long n) {
-    long long max_prime = -1;
-
-    while (n % 2 == 0) {
-        max_prime = 2;
-        n /= 2;
-    }
-
-    for (long long i = 3; i <= sqrt(n); i += 2) {
-        while (n % i == 0) {
-            max_prime = i;
-            n /= i;
-        }
-    }
-
-    if (n > 2) {
-        max_prime = n;
-    }
-
-    return max_prime;
-}
