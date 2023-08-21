bf229dee39b3d62e13d8ab2079ca5592032214c5
Aughostinos <augustinos.nabil@gmail.com>
2023-08-21 11:19:24 -0700

    task0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - updates the value it points to to 98
+ * @n: input
 */
 void reset_to_98(int *n)
 {
