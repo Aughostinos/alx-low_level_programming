1a1799f357e1a662405bb6bf636c62bdd11a1f1c
Aughostinos <augustinos.nabil@gmail.com>
2023-08-21 14:57:30 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,14 +3,15 @@
 /**
  * _strlen - calclate the lenght of a string
  * @s: string input
+ * Return: 0
 */
 int _strlen(char *s)
 {
 	int n;
 
-	for(n = 0; *s != '\0'; s++)
+	for (n = 0; *s != '\0'; s++)
 	{
 	n++;
 	}
-    return (n);
+	return (n);
 }
