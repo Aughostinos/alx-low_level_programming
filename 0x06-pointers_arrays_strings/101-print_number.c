65398c9a496237f7f8c6bfd27fb0c36dbcb35196
Aughostinos <augustinos.nabil@gmail.com>
2023-08-24 16:17:34 -0700

    fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,12 +12,12 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		n* = -1;
 	}
 
-	x = n;
+	i = n;
 
-	if (x / 10)
-		print_number(x / 10);
-	_putchar(x % 10 + '0');
+	if (i / 10)
+		print_number(i / 10);
+	_putchar(i % 10 + '0');
 }
