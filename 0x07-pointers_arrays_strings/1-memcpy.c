ee838f3c504ed436b6572191b59060f82bb3638b
Aughostinos <augustinos.nabil@gmail.com>
2023-08-28 13:17:14 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
  *_memcpy - copies memory area
  *@dest: destenation
  *@src: source
  *@n: bytes' number
  *Return: pointer to dest
- */
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		dest[i] = src[i];
