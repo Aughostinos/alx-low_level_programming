69211280db7ba0b158fc3d9829061c5446a2d5bf
Aughostinos <augustinos.nabil@gmail.com>
2023-08-30 19:08:11 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * is_prime_number - check if the number is prime
+ * @n: number to evaluate
+ * Return: 1 or 0
+ */
+int real_prime(int n, int i);
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * real_prime - recursive function 
+ * @n: input
+ * @i: input
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int real_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (real_prime(n, i - 1));
+}
