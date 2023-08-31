a3b6c215285cb7b8c8c03e0226a9285d98725b6e
Aughostinos <augustinos.nabil@gmail.com>
2023-08-30 18:58:44 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *  _sqrt_recursion - returns the natural square root of a number
+ *  @n: input number
+ *  Return: n power 2 or -1
+ */
+int sqr_rec(int j, int i);
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqr_rec(n, 0));
+}
+
+/**
+ * sqr_rec - squere
+ * @n: input bumber
+ * @i: input
+ * Return: the resulting square root
+ */
+int sqr_rec(int j, int i)
+{
+	if (i * i > j)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (sqr_rec(j, i + 1));
+}
