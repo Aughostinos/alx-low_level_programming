ec08771e644936a2f5a109352df1ce1038d2e2da
Aughostinos <augustinos.nabil@gmail.com>
2023-09-01 19:49:54 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,36 +6,35 @@
  * @argv: vector
  * Return: 0
  */
-int main(int argc, char *argv[]) {
-    // Initialize a variable to store the sum
-    int sum = 0;
+int main(int argc, char *argv[])
 
-    // Check if there are any command-line arguments
-    if (argc == 1) {
-        // No arguments provided, print 0 and return
-        printf("0\n");
-        return 0;
-    }
+{
 
-    // Loop through the arguments starting from argv[1] (argv[0] is the program name)
-    int i;
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-    for (i = 1; i < argc; i++) {
-        // Attempt to convert the argument to an integer
-        int num = atoi(argv[i]);
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
 
-        // Check if the conversion was successful and the number is positive
-        if (num <= 0) {
-            // Invalid input, print Error and return 1
-            printf("Error\n");
-            return 1;
-        }
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
 
-        // Add the valid positive number to the sum
-        sum += num;
-    }
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 
-    // Print the result and return 0
-    printf("%d\n", sum);
-    return 0;
+		count++;
+	}
+
+	printf("%d\n", sum); /*print sum*/
+
+	return (0);
 }
+
