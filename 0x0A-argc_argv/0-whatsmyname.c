9019130c1946b3e74b3f61391d607a627e5f7400
Aughostinos <augustinos.nabil@gmail.com>
2023-09-01 19:00:05 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: counter
+ * @argv: vector
+ * Return: 0
+ */
+int main (int argc, char *argc[])
+{
+	printf("%s\n", *argv[0]);
+	return (0);
+}
