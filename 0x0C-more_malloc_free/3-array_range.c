d0de303bc165e9b381441da506418a6c8f5a3a9e
Aughostinos <augustinos.nabil@gmail.com>
2023-09-08 15:31:44 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -4,7 +4,7 @@
 /**
  * *array_range - creates an array of integers
  * @min: input
- * @max: input 
+ * @max: input
  * Return: pointer
 */
 int *array_range(int min, int max)
@@ -13,17 +13,21 @@ int *array_range(int min, int max)
 	int i, size;
 
 	if (min > max)
+	{
 		return (NULL);
-
+	}
 	size = max - min + 1;
 
 	p = malloc(sizeof(int) * size);
 
 	if (p == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; min <= max; i++)
+	{
 		p[i] = min++;
-
+	}
 	return (p);
 }
