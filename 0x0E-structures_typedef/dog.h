0855420344dca54f344146d4712c92ecdc05ff5b
Aughostinos <augustinos.nabil@gmail.com>
2023-09-11 15:38:56 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog profile
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
