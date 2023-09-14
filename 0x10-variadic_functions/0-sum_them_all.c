8f7a8c98af2b4ad512471f23b0166a4031ac4916
Aughostinos <augustinos.nabil@gmail.com>
2023-09-14 08:03:13 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,16 +4,18 @@
 /**
  * sum_them_all - function that returns the sum of all its parameters.
  * @n: parameter number
+ * @...: params
  * Return: sum or 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
 	if (n == 0)
 		return (0);
-	
+
 	int sum = 0;
 
-	va_list args; 
+	va_list args;
+
 	va_start(args, n);
 
 	unsigned int i = 0;
@@ -21,6 +23,7 @@ int sum_them_all(const unsigned int n, ...)
 	while (i < n)
 	{
 		int args = va_arg(args, int);
+
 		sum += args;
 		i++;
 	}
