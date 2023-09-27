0f1443b6007c6f0a7840035792e4a75485feee81
Aughostinos <augustinos.nabil@gmail.com>
2023-09-26 17:25:07 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,39 +1,34 @@
 #include "lists.h"
-#include <stdint.h>
-#include <stddef.h>
-#include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a listint_t linked list safely
- * avoiding issues with loops.
- * @h: Pointer to a pointer to the head of the list.
- * Return: The size of the list that was freed.
+ * free_listint_safe - Frees a listint_t linked list.
+ * @h: A pointer to a pointer to the head of the list.
+ *
+ * Returns: The size of the list that was freed.
+ * The function sets the head to NULL.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current = *h;
-	size_t count = 0;
-	uintptr_t hash_table[1024] = {0};
+	size_t size = 0;
+	listint_t *current, *next_node;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current = *h;
 
 	while (current != NULL)
 	{
-		uintptr_t current_address = (uintptr_t)current;
+		size++;
+		next_node = current->next;
+		free(current);
+		current = next_node;
 
-		if (hash_table[current_address % 1024] == current_address)
-		{
-			return (count);
-		}
-
-	listint_t *next = current->next;
-
-	free(current);
-	count++;
-
-	hash_table[current_address % 1024] = current_address;
-
-	current = next;
+		if (current == *h)
+		break;
 	}
 
 	*h = NULL;
-	return (count);
+
+	return (size);
 }
