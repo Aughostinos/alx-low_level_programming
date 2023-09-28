a2911a37679d5ec0003fe7e423161c9b3b6466e0
Aughostinos <augustinos.nabil@gmail.com>
2023-09-28 15:08:15 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,13 +11,15 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 
 	while (*b != '\0')
 	{
-		result = result * 2 + *b;
-		b++
+		if (*b != '0' && *b != '1')
+			return (0);
+		result = result * 2 + (*b - '0');
+		b++;
 	}
 	return (result);
 }
