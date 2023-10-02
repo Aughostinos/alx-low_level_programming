b8b2ac62e01e5058462173a7e0830fb9ebdb6df0
Aughostinos <augustinos.nabil@gmail.com>
2023-10-02 14:44:14 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,34 @@
 #include "main.h"
+
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: The name of the file.
- * @text_content: The NULL terminated string to add at the end of the file.
+ * create_file - Creates a file and writes text content to it.
+ * @filename: The name of the file to create.
+ * @text_content: The NULL terminated string to write to the file.
  *
  * Return: 1 on success, -1 on failure.
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	FILE *file;
-	size_t len = 0;
+	int fd, write_result;
+	mode_t file_permissions = S_IRUSR | S_IWUSR;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	file = fopen(filename, "a");
-	if (file == NULL)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, file_permissions);
+	if (fd == -1)
 		return (-1);
 
-	while (text_content[len])
-		len++;
-
-	if (fwrite(text_content, 1, len, file) != len)
+	if (text_content != NULL)
 	{
-		fclose(file);
-		return (-1);
+		write_result = write(fd, text_content, strlen(text_content));
+		if (write_result == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
-	fclose(file);
+	close(fd);
 	return (1);
 }
