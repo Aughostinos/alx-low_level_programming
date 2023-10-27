273cf40060aff8f7c7ccc0370b298aa47e0b45d4
aughostinos <augustinos.nabil@gmail.com>
2023-10-26 18:32:36 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,14 +10,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned bit_index;
-	unsigned desired_bit;
-
 	/*check if index out of range*/
 	if (index < (sizeof(unsigned long int) * 8))
-		bit_index = n >> index;
-		desired_bit = bit_index & 1;
-		return (desired_bit);
+		return ((n >> index) & 1);
 	return (-1);
 
 }
