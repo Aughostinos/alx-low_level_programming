23cc21874bb564f02d8230d4693df78c6fd0645a
aughostinos <augustinos.nabil@gmail.com>
2023-10-30 18:57:17 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0666);
+	_file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (_file == -1)
 		return (-1);
 
