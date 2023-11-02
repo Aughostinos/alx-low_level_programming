a2b90fba29578a16df4ad650ff308d1e66ba4ca6
aughostinos <augustinos.nabil@gmail.com>
2023-11-02 04:27:08 -0700

    fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	_file = open(filename, O_WRONLY | O_APPEND);
+	_file = fopen(filename, O_WRONLY | O_APPEND);
 	if (_file == -1)
 		return (-1);
 
