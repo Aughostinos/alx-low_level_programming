d459a682dd8bd2a7545bbee455270f101f46d04a
aughostinos <augustinos.nabil@gmail.com>
2023-12-07 14:15:11 -0800

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * dlistint_len - a function that returns the number 
+ * dlistint_len - a function that returns the number
  * of elements in a linked dlistint_t list.
  *
  * @h: input
