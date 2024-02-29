927e0500e363b2f169a49b2e0087a29ddfefb266
aughostinos <augustinos.nabil@gmail.com>
2024-02-29 14:49:34 -0800

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,7 +7,7 @@ Module to find the perimeter of an island in a grid.
 def island_perimeter(grid):
     """
      that returns the perimeter of the island described in grid
-     """0
+     """
     row = len(grid)
     coloum = len(grid[0])
     perimeter = 0
@@ -27,5 +27,4 @@ def island_perimeter(grid):
 
                 if (j == coloum-1) or (grid[i][j+1] == 0):
                     perimeter += 1
-
-    print(perimeter)
+    return perimeter
